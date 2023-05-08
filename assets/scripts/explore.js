9a5510d741ffa5f3d0c357ee29112cae2034c3b8
baothy8155 <baothy.nguyen815@gmail.com>
2023-05-07 22:23:26 -0700

    attempted to work on explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,5 +4,30 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
    
-    
+    const button = document.querySelector('button');
+    const select = document.querySelector('select');
+    const textArea = document.querySelector('textarea');
+    const img = document.querySelector('img');
+    const voiceSelction = document.querySelector('#voice-select');
+    const synth = window.speechSynthesis;
+
+let voices = [];
+
+function populateVoiceList() {
+  voices = synth.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+}
+
+populateVoiceList();
+const utterThis = new SpeechSynthesisUtterance(textArea.value);
+
 }
